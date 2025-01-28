8263a64a8daeeb4dfd72f1b63c95ca3b1d14f3b2
maddog19527 <mjgriffin19527@gmail.com>
2025-01-28 02:08:13 -0500

    changes

---
+++ Functions/Tensorflow.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
